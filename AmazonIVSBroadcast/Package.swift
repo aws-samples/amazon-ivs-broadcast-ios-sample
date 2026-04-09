c4107f927c93519a7d103edd50b15e3321554e20
Caleigh Minshall <cminshal@amazon.com>
2026-04-09 10:29:22 -0700

    Update Broadcast SDK to 1.41.0
    cr: https://code.amazon.com/reviews/CR-266214583

--- AmazonIVSBroadcast/Package.swift
+++ AmazonIVSBroadcast/Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonIVSBroadcast",
-            url: "https://broadcast.live-video.net/1.40.0/AmazonIVSBroadcast.xcframework.zip",
-            checksum: "864a655c37d90b59e7c1ea75bc8ef9a4ee436df0329ee2b96677aa22541cdfdb"
+            url: "https://broadcast.live-video.net/1.41.0/AmazonIVSBroadcast.xcframework.zip",
+            checksum: "5ea9228572e088de824d27263fec503ce9d78a78c053083988098a6809834710"
         ),
         .binaryTarget(
             name: "AmazonIVSBroadcastStages",
-            url: "https://broadcast.live-video.net/1.40.0/AmazonIVSBroadcast-Stages.xcframework.zip",
-            checksum: "0795f2f473330873682a3b3d68692b0c2e16dcdb0735e636514033ec63bb7398"
+            url: "https://broadcast.live-video.net/1.41.0/AmazonIVSBroadcast-Stages.xcframework.zip",
+            checksum: "cfea8b1f0312cbdf4f522224e2843e08a8afb79abc7f47348726eb4a8400dbd6"
         )
     ]
 )
