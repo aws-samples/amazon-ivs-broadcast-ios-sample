452f835808e79040ec85b203e13b1dd0a9ccb97d
IVS Release Automation <SRVC-VideoPlayback-Automation@justin.tv>
2026-05-07 22:00:44 +0900

    Update AmazonIVSBroadcast to version 1.42.0
    cr: https://code.amazon.com/reviews/CR-272789485

--- AmazonIVSBroadcast/Package.swift
+++ AmazonIVSBroadcast/Package.swift
@@ -20,13 +20,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonIVSBroadcast",
-            url: "https://broadcast.live-video.net/1.41.0/AmazonIVSBroadcast.xcframework.zip",
-            checksum: "5ea9228572e088de824d27263fec503ce9d78a78c053083988098a6809834710"
+            url: "https://broadcast.live-video.net/1.42.0/AmazonIVSBroadcast.xcframework.zip",
+            checksum: "2af2d1808dff65b0aaeee7f59da396841d26dffe4c70f956a30d68e2cc07805d"
         ),
         .binaryTarget(
             name: "AmazonIVSBroadcastStages",
-            url: "https://broadcast.live-video.net/1.41.0/AmazonIVSBroadcast-Stages.xcframework.zip",
-            checksum: "cfea8b1f0312cbdf4f522224e2843e08a8afb79abc7f47348726eb4a8400dbd6"
+            url: "https://broadcast.live-video.net/1.42.0/AmazonIVSBroadcast-Stages.xcframework.zip",
+            checksum: "3399b53b60871bce070ac598d89ed77f7181f05ee9dd6faabd3fffdf19051250"
         )
     ]
 )
